59d4d8474f2ec2074c4330c0994258d7f3e1ddfa
Jakob Sack <mail@jakobsack.de>
2016-02-02 14:12:34 +0100

    Release 4.0.0-alpha4

--- lib/tilia/v_object/version.rb
+++ lib/tilia/v_object/version.rb
@@ -3,7 +3,7 @@ module Tilia
     # This class contains the version number for the VObject package.
     class Version
       # Full version number.
-      VERSION = '4.0.0-alpha3'
+      VERSION = '4.0.0-alpha4'
     end
   end
 end
